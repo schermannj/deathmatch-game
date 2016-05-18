3882346c9a568e3706f270466e860dd9c6ce0b00
Vetal <v.ryabenko@aimprosoft.com>
2016-05-18 13:30:12 +0300

    refactored

--- back-end/app/deathmatch.js
+++ back-end/app/deathmatch.js
@@ -19,11 +19,11 @@ module.exports = function (io, socket) {
     //events
     gameSocket.on('createRoom', createRoomEvent);
     gameSocket.on('joinRoom', joinRoomEvent);
-    gameSocket.on('playerIsReady', playerIsReadyEvent);
     gameSocket.on('refreshRoom', refreshRoom);
-    gameSocket.on('answer', answerEvent);
-    gameSocket.on('getQuestion', getQuestionEvent);
+    gameSocket.on('playerIsReady', playerIsReadyEvent);
     gameSocket.on('allPlayersAreReady', allPlayersAreReady);
+    gameSocket.on('getQuestion', getQuestionEvent);
+    gameSocket.on('answer', answerEvent);
     gameSocket.on('getTableScore', getTableScore);
 };
 
@@ -41,42 +41,28 @@ function createRoomEvent(data) {
         socket: sock.id,
         score: 0,
         finish: false
-    }).save(function (err, player) {
-        validate(err, "Can't save player.");
+    }).save()
+        .then(function (player) {
 
-        new Game({
-            _id: uuid.v1({nsecs: 961}),
-            players: [
-                player._id
-            ],
-            questions: [],
-            level: data.level ? data.level : 1
-        }).save(function (err, game) {
-                validate(err, "Can't save game.");
+            return new Game({
+                    _id: uuid.v1({nsecs: 961}),
+                    players: [
+                        player._id
+                    ],
+                    questions: [],
+                    level: data.level ? data.level : 1
+                }
+            ).save()
+                .then(function (game) {
+                    // Join the Game and wait for the players
+                    sock.join(game._id);
 
-                // Join the Game and wait for the players
-                sock.join(game._id);
-
-                sock.emit('roomCreated', {
-                    game: game._id,
-                    you: player
-                });
-            }
-        );
-    });
-}
-
-function getPlayers(game) {
-    var deferred = q.defer();
-
-    Player.find({_id: {$in: game.players}}, function (err, players) {
-        validate(err, "Can't find player.");
-        if (err) deferred.reject(err);
-
-        deferred.resolve(players);
-    });
-
-    return deferred.promise;
+                    sock.emit('roomCreated', {
+                        game: game._id,
+                        you: player
+                    });
+                }, validate);
+        }, validate)
 }
 
 function joinRoomEvent(data) {
@@ -88,7 +74,6 @@ function joinRoomEvent(data) {
 
     // If the game exists...
     if (game) {
-
         //create new player
         new Player({
             _id: uuid.v1({nsecs: 961}),
@@ -98,18 +83,15 @@ function joinRoomEvent(data) {
             socket: sock.id,
             score: 0,
             finish: false
-        }).save(function (err, nextPlayer) {
-            validate(err, "Can't save player.");
-
-            Game.findOneAndUpdate(
-                {_id: data.game},
-                {
-                    $push: {players: nextPlayer._id}
-                },
-                {"new": true},
-                function (err, game) {
-                    validate(err, "Can't find game.");
-
+        }).save()
+            .then(function (nextPlayer) {
+                Game.findOneAndUpdate(
+                    {_id: data.game},
+                    {
+                        $push: {players: nextPlayer._id}
+                    },
+                    {"new": true}
+                ).then(function (game) {
                     // Join the game
                     sock.join(data.game);
 
@@ -117,26 +99,115 @@ function joinRoomEvent(data) {
                         game: game._id,
                         you: nextPlayer
                     });
-                });
-        });
+                }, validate);
+            }, validate);
     } else {
         // Otherwise, send an error message back to the player.
         this.emit('error', {message: "This game does not exist."});
     }
 }
 
+function refreshRoom(req) {
+    Game.findOne({_id: req.game}).then(function (game) {
+
+        getPlayers(game).then(function (players) {
+
+            gameIo.sockets.in(req.game).emit('updateRoom', {
+                game: game._id,
+                players: players
+            });
+        }, validate);
+    }, validate);
+}
+
+function playerIsReadyEvent(data) {
+    Game.findOne({_id: data.game}).then(function (game) {
+
+        if (_.contains(game.players, data.player._id)) {
+
+            Player.findOneAndUpdate({_id: data.player._id}, {$set: {ready: true}}).then(function () {
+
+                getPlayers(game).then(function (players) {
+
+                    gameIo.sockets.in(game._id).emit('updateRoom', {players: players});
+
+                }, validate);
+            }, validate);
+        }
+    }, validate)
+}
+
+function allPlayersAreReady(data) {
+    //TODO: implement ability to choose levels
+    loadQuestionsForGame(1)
+        .then(function (questions) {
+
+            return Game.findOneAndUpdate(
+                {_id: data.game},
+                {$set: {questions: get5RandomQuestionsIds(questions)}}
+            )
+
+        }, validate)
+        .then(function (game) {
+
+            startCountdown(game).then(function () {
+                gameIo.sockets.in(game._id).emit('startTheBattle');
+            })
+
+        }, validate);
+}
+
+function getQuestionEvent(req) {
+    Game.findOne({_id: req.game}).then(function (game) {
+        assertNotNull(game);
+
+        var qId = null;
+
+        if (req.qIndex < game.questions.length) {
+            qId = game.questions[req.qIndex];
+        } else {
+            validate(new Error("Wrong qIndex: " + req.qIndex));
+        }
+
+        //if it's a valid question index and there is next question
+        if (qId != null) {
+
+            Question.findOne({_id: qId}).then(function (question) {
+                var startScore = 60000;
+
+                Player.findOne({_id: req.player._id}).then(function (player) {
+
+                    gameIo.sockets.in(game._id).sockets[player.socket].emit('receiveQuestion', {
+                        question: {
+                            id: question._id,
+                            text: question.question,
+                            possibleAnswers: question.possibleAnswers,
+                            isRadio: question.isRadio
+                        },
+                        qScore: startScore,
+                        totalScore: player.score
+                    });
+
+                    putScoreToMap(player.socket, startScore);
+
+                    startScoreCountdown(game._id, player.socket, startScore);
+
+                }, validate);
+            }, validate)
+        }
+    }, validate)
+}
+
 function answerEvent(req) {
     pSocketsScoreMap[req.player.socket].inAction = false;
 
-    Game.findOne({_id: req.game}, function (err, game) {
-        validate(err, "Can't find game.");
+    Game.findOne({_id: req.game}).then(function (game) {
         assertNotNull(game);
 
         var pScore = pSocketsScoreMap[req.player.socket].score;
         var isCorrect = false;
 
-        Question.findOne({_id: req.q._id}, function (err, question) {
-            validate(err, "Can't find question.");
+        Question.findOne({_id: req.q._id}).then(function (question) {
 
             var answersIntersection = _.intersection(question.rightAnswers, req.q.answer);
 
@@ -153,11 +224,11 @@ function answerEvent(req) {
                 updateDocument['$set'] = {finish: true};
             }
 
-            Player.update({_id: req.player._id}, updateDocument, function (err) {
-                validate(err, "Can't find player.");
-
-                Player.findOne({_id: req.player._id}, function (err, player) {
-
+            Player.update({_id: req.player._id}, updateDocument)
+                .then(function () {
+                    return Player.findOne({_id: req.player._id});
+                }, validate)
+                .then(function (player) {
                     pSocketsScoreMap[req.player.socket].score = 0;
 
                     if (hasMoreQuestions) {
@@ -177,119 +248,21 @@ function answerEvent(req) {
                         //send request to update score table data for other users
                         gameIo.sockets.in(req.game).emit('doRefreshCycle');
                     }
-                });
-            });
-        });
-    });
-}
-
-function getQuestionEvent(req) {
-    Game.findOne({_id: req.game}, function (err, game) {
-        validate(err, "Can't find game.");
-        assertNotNull(game);
-
-        var qId = null;
-
-        if (req.qIndex < game.questions.length) {
-            qId = game.questions[req.qIndex];
-        } else {
-            throw new Error("Wrong qIndex: " + req.qIndex);
-        }
-
-        //if it's a valid question index and there is next question
-        if (qId != null) {
-            Question.findOne({_id: qId}, function (err, question) {
-                validate(err, "Can't find question.");
-
-                var startScore = 60000;
-
-                Player.findOne({_id: req.player._id}, function (err, player) {
-                    validate(err, "Can't find player.");
-
-                    gameIo.sockets.in(game._id).sockets[player.socket].emit('receiveQuestion', {
-                        question: {
-                            id: question._id,
-                            text: question.question,
-                            possibleAnswers: question.possibleAnswers,
-                            isRadio: question.isRadio
-                        },
-                        qScore: startScore,
-                        totalScore: player.score
-                    });
-
-                    putScoreToMap(player.socket, startScore);
-                    startScoreCountdown(game._id, player.socket, startScore);
-                });
-            })
-        }
-    })
-}
-
-function putScoreToMap(pSocket, score) {
-    pSocketsScoreMap[pSocket] = {
-        score: score,
-        inAction: true
-    }
-}
-
-function refreshRoom(req) {
-    Game.findOne({_id: req.game}, function (err, game) {
-        validate(err, "Can't find game.");
-
-        getPlayers(game).then(function (players) {
-            validate(err, "Can't find players.");
-
-            gameIo.sockets.in(req.game).emit('updateRoom', {
-                game: game._id,
-                players: players
-            });
-        });
-    });
-}
-
-function playerIsReadyEvent(data) {
-    Game.findOne({_id: data.game}, function (err, game) {
-        validate(err, "Can't find game.");
-
-        if (_.contains(game.players, data.player._id)) {
-            Player.findOneAndUpdate({_id: data.player._id}, {$set: {ready: true}}, function (err, player) {
-                validate(err, "Can't find player.");
-
-                getPlayers(game).then(function (players) {
-                    gameIo.sockets.in(game._id).emit('updateRoom', {players: players});
-                });
-            });
-        }
-    })
-}
-
-function allPlayersAreReady(data) {
-    //TODO: fix level
-    loadQuestionsForGame(1).then(function (questions) {
-        Game.findOneAndUpdate(
-            {_id: data.game},
-            {$set: {questions: get5RandomQuestionsIds(questions)}},
-            function (err, game) {
-                validate(err, "Can't find game.");
-
-                startCountdown(game).then(function () {
-                    gameIo.sockets.in(game._id).emit('startTheBattle');
-                });
-            }
-        );
-    });
+                }, validate);
+        }, validate);
+    }, validate);
 }
 
 function getTableScore(req) {
     var sock = this;
 
-    Game.findOne({_id: req.game}, function (err, game) {
-        validate(err, "Can't find game.");
-        assertNotNull(game);
+    Game.findOne({_id: req.game})
+        .then(function (game) {
+            assertNotNull(game);
 
-        Player.find({_id: {$in: game.players}, finish: true}, function (err, players) {
-            validate(err, "Can't find players");
-
+            return Player.find({_id: {$in: game.players}, finish: true})
+        }, validate)
+        .then(function (players) {
             //collect score table data
             var scoreTableData = _.map(players, function (player) {
                 return {
@@ -302,8 +275,18 @@ function getTableScore(req) {
             sock.emit('refreshScoreTable', {
                 players: scoreTableData
             })
-        })
-    });
+        }, validate);
+}
+
+function getPlayers(game) {
+    return Player.find({_id: {$in: game.players}});
+}
+
+function putScoreToMap(pSocket, score) {
+    pSocketsScoreMap[pSocket] = {
+        score: score,
+        inAction: true
+    }
 }
 
 function startCountdown(game) {
@@ -344,15 +327,7 @@ function startScoreCountdown(game, pSocket, score) {
 }
 
 function loadQuestionsForGame(level) {
-    var deferred = q.defer();
-
-    Question.find({level: level}, function (err, questions) {
-        validate(err, "Can't find question.");
-
-        deferred.resolve(questions);
-    });
-
-    return deferred.promise;
+    return Question.find({level: level});
 }
 
 function get5RandomQuestionsIds(questions) {
@@ -437,9 +412,9 @@ function mongoQuestionsDump() {
     }).save();
 }
 
-function validate(err, message) {
+function validate(err) {
     if (err) {
-        throw new Error(message.concat("Cause: " + err));
+        throw new Error("Cause: " + err);
     }
 }
 
