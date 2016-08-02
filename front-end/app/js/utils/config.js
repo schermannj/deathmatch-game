d831c3e4456e8fb9f4fd3d4b4bffeb3552c860da
Vetal <v.ryabenko@aimprosoft.com>
2016-08-02 17:48:47 +0300

    remove old front-end module

--- front-end/app/js/utils/config.js
+++
@@ -1,9 +0,0 @@
-angular.module('app.config', [])
-    .constant('Config', config());
-
-function config() {
-    return {
-        SERVER_URL: 'http://localhost:3000',
-        WS_SERVER_URL : 'http://localhost:3001/'
-    }
-}
