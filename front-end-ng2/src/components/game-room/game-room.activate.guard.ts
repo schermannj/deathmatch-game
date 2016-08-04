c419fb483aa2c7e63d52d42cd9026367946761d5
Vetal <v.ryabenko@aimprosoft.com>
2016-08-04 13:39:41 +0300

    fixed game deactivator

--- front-end-ng2/src/components/game-room/game-room.activate.guard.ts
+++ front-end-ng2/src/components/game-room/game-room.activate.guard.ts
@@ -1,6 +1,7 @@
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from "@angular/router";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
+import {STORAGE_KEYS} from "../../util/config.util";
 
 @Injectable()
 export class GameRoomActivateGuard implements CanActivate {
@@ -9,6 +10,7 @@ export class GameRoomActivateGuard implements CanActivate {
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>|boolean {
+        localStorage.setItem(STORAGE_KEYS.CAN_DEACTIVATE_GAME_ROOM, String(false));
 
         return true;
     }
--- front-end-ng2/src/components/game-room/game-room.deactivate.guard.ts
+++ front-end-ng2/src/components/game-room/game-room.deactivate.guard.ts
@@ -3,24 +3,31 @@ import {GameRoomComponent} from "./game-room.component";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
 import {STATE_STATUS, STORAGE_KEYS} from "../../util/config.util";
+import {SocketService} from "../../services/socket.service";
 
 @Injectable()
 export class GameRoomDeactivateGuard implements CanDeactivate<GameRoomComponent> {
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private socket: SocketService) {
     }
 
     canDeactivate(component: GameRoomComponent,
                   route: ActivatedRouteSnapshot,
                   state: RouterStateSnapshot): Observable<boolean>|boolean {
 
+        if (Boolean(localStorage.getItem(STORAGE_KEYS.CAN_DEACTIVATE_GAME_ROOM))) {
+            return true;
+        }
+
         if (localStorage.getItem(STORAGE_KEYS.STATE) !== STATE_STATUS.FINISHED) {
+            this.socket.io().removeAllListeners();
             this.router.navigate(['/']);
+            localStorage.setItem(STORAGE_KEYS.CAN_DEACTIVATE_GAME_ROOM, String(true));
 
             return false;
         }
 
-        return false;
+        return true;
     }
 
 }
